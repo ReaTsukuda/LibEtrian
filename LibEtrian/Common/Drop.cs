97eed6afd9d3f9eb870bb547a869a8b7638a2cd3
Rea <1171053+ReaTsukuda@users.noreply.github.com>
2025-08-20 11:00:10 -0700

    enemydata parsing

---
+++ LibEtrian/Common/Drop.cs
@@ -0,0 +1,27 @@
+﻿namespace LibEtrian.Common;
+
+/// <summary>
+/// An item an enemy can drop.
+/// </summary>
+public class Drop
+{
+  /// <summary>
+  /// The ID of the item for this drop.
+  /// </summary>
+  public U16 ItemId { get; init; }
+  
+  /// <summary>
+  /// The chance for this item to drop, if its condition is fulfilled.
+  /// </summary>
+  public U16 Chance { get; init; }
+  
+  /// <summary>
+  /// An argument value for the conditional drop requirement.
+  /// </summary>
+  public U8 RequirementArg { get; init; }
+  
+  /// <summary>
+  /// The conditional drop requirement type.
+  /// </summary>
+  public U8 RequirementType { get; init; }
+}
---
+++ LibEtrian/Common/StatBlock.cs
@@ -0,0 +1,47 @@
+﻿namespace LibEtrian.Common;
+
+/// <summary>
+/// A block of stats, shared between players and enemies.
+/// </summary>
+public class StatBlock
+{
+  /// <summary>
+  /// An entity's max HP.
+  /// </summary>
+  public S32 MaxHp { get; init; }
+  
+  /// <summary>
+  /// An entity's max TP.
+  /// </summary>
+  public S32 MaxTp { get; init; }
+  
+  /// <summary>
+  /// An entity's STR.
+  /// </summary>
+  public S16 Str { get; init; }
+  
+  /// <summary>
+  /// An entity's VIT.
+  /// </summary>
+  public S16 Vit { get; init; }
+  
+  /// <summary>
+  /// An entity's AGI.
+  /// </summary>
+  public S16 Agi { get; init; }
+  
+  /// <summary>
+  /// An entity's LUC.
+  /// </summary>
+  public S16 Luc { get; init; }
+  
+  /// <summary>
+  /// An entity's TEC.
+  /// </summary>
+  public S16 Tec { get; init; }
+  
+  /// <summary>
+  /// An entity's WIS. Exists as padding in EO3 through EO2U, actually used in EO5 and EON.
+  /// </summary>
+  public S16 Wis { get; init; }
+}
