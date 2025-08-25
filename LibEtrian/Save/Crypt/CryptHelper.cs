fe5807bc7f72591ee73f36f5fd463de1f01abbd7
Rea <1171053+ReaTsukuda@users.noreply.github.com>
2025-08-24 23:53:07 -0700

    Very early save parsing support

---
+++ LibEtrian/Save/Crypt/CryptHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace LibEtrian.Save.Crypt;
+
+/// <summary>
+/// For dealing with the save encryption on EO HD saves.
+/// </summary>
+public static class CryptHelper
+{
+  // AES configuration values.
+  private const string Key = "Atlus-inc-SQS3SE";
+  private const string Iv = "Atlus-inc-SQS3Se";
+  private const S32 BlockSize = 128;
+  private const S32 KeySize = 128;
+
+  public static U8[] Decrypt(U8[] encrypted)
+  {
+    var aes = InitAes();
+    return aes.DecryptCbc(encrypted, aes.IV);
+  }
+
+  public static U8[] Encrypt(U8[] unencrypted)
+  {
+    var aes = InitAes();
+    return aes.DecryptCbc(unencrypted, aes.IV);
+  }
+
+  private static Aes InitAes()
+  {
+    var aes = Aes.Create();
+    aes.BlockSize = BlockSize;
+    aes.KeySize = KeySize;
+    aes.Key = Encoding.UTF8.GetBytes(Key);
+    aes.IV = Encoding.UTF8.GetBytes(Iv);
+    // Mode and Padding are CBC and PKCS7, the defaults for AES, so no need to set them.
+    return aes;
+  }
+}
---
+++ LibEtrian/Save/EO2UGameSave.cs
@@ -0,0 +1,13 @@
+﻿namespace LibEtrian.Save;
+
+/// <summary>
+/// mo2r##_game.sav. Contains the bulk of what people who want to edit their save will care about: party data, money,
+/// inventory/shop state, completion state.
+/// </summary>
+public class EO2UGameSave
+{
+  /// <summary>
+  /// The save's file identifier, located at the very top of the file.
+  /// </summary>
+  private const string SaveIdentifier = "MO2RGAME";
+}
